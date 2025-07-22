992ed07b08c5fa2325e05112d7ae96f337582431
Patrick B case <pcase818@gmail.com>
2025-07-21 23:34:21 -0400

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,5 @@
+Project Red Pill Hive - Phase 1 (Free Deployment)
+1. Upload all files to GitHub Pages or any static host.
+2. Default passphrase is: vault-echo-4273
+3. Link your Gumroad to the index.html subscribe button.
+4. Optional: Connect portablepi.crypto to your GitHub Pages domain.
---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBPbL1zmbEpyBKcxJMjreuqlfteRl6qjjk",
+  authDomain: "redpill-hive.firebaseapp.com",
+  databaseURL: "https://redpill-hive-default-rtdb.firebaseio.com/",
+  projectId: "redpill-hive",
+  storageBucket: "redpill-hive.appspot.com",
+  messagingSenderId: "870856030107",
+  appId: "1:870856030107:web:8ddc614be76c64a21a133e",
+  measurementId: "G-LM2T58WTG6"
+};
